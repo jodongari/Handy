e4ab343768cea34d5edbb0361e194d3daae1be83
macho <macho@gmarket.com>
2022-06-26 12:47:45 +0900

    initial commit

---
+++ src/main/java/com/jodongari/handy/HandyApplication.java
@@ -0,0 +1,13 @@
+package com.jodongari.handy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jodongari/handy/HandyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodongari.handy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
