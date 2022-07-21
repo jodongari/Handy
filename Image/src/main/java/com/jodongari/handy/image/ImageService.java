095ba16df035e8bcc4e4cb0c0ffc818551b0f105
unknown <jjangman821@gmail.com>
2022-07-21 22:59:32 +0900

    HANDY-30 S3 업로드 메서드명, 파라미터, 변수명 변경

--- Image/src/main/java/com/jodongari/handy/image/ImageService.java
+++ Image/src/main/java/com/jodongari/handy/image/ImageService.java
@@ -17,18 +17,19 @@ public class ImageService {
     private static final String HANDY_IMAGE_BUCKET_NAME = "handy-image";
     private static final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
 
-    public String putObject(String storePK, File imageFile){
+    public String uploadObjectToS3(File imageFile){
         // TODO : Extension 검사 코드 구현
 
-        String imageUrl = storePK + "/" + Hashing.sha256().hashString(storePK + System.currentTimeMillis(), StandardCharsets.UTF_8);
+        String objectUrl = String.valueOf(Hashing.sha256().hashString(String.valueOf(System.currentTimeMillis()), StandardCharsets.UTF_8));
 
         try {
-            s3.putObject(HANDY_IMAGE_BUCKET_NAME, imageUrl, imageFile);
+            s3.putObject(HANDY_IMAGE_BUCKET_NAME, objectUrl, imageFile);
         } catch (AmazonServiceException e) {
             // TODO : 커스텀 익셉션으로 내려보내서 어떤 에러인지 파악 가능하도록 변경할 것
             log.error(e.getErrorMessage());
         }
-        return imageUrl;
+
+        return objectUrl;
     }
 
 }
