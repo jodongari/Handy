bc2a4d332f2f33e2f432cc147a9a871c9e9f75ad
jaehyun kim <kimjaehyun9409@gmail.com>
2022-10-22 16:34:36 +0900

    HANDY-44 Store 도메인 리팩터링

--- Store/src/main/java/com/jodongari/handy/protocol/dto/response/GetStoreResponse.java
+++
@@ -1,9 +0,0 @@
-package com.jodongari.handy.protocol.dto.response;
-
-import com.jodongari.handy.domain.store.Store;
-import lombok.Value;
-
-@Value
-public class GetStoreResponse {
-    Store store;
-}
--- Store/src/main/java/com/jodongari/handy/protocol/dto/response/RegisterStoreResponseDto.java
+++ Store/src/main/java/com/jodongari/handy/protocol/dto/response/RegisterStoreResponseDto.java
@@ -6,47 +6,47 @@ import lombok.Value;
 
 @Value
 public class RegisterStoreResponseDto {
-//    Long seq;
-//    Long ownerSeq;
-//    String name;
-//    String businessReportCardImage;
-//    String businessLicenseImage;
-//    String businessName;
-//    String businessPersonName;
-//    String businessNumber;
-//    String businessAddress;
-//    String address;
-//    String telNumber;
-//    String introduction;
-//    String openTime;
-//    String dayOff;
-//    String originCountry;
-//    String logo;
-//    String backgroundImage;
-//    Integer tableCount;
-//    String category;
-//    StoreStatus status;
-//
-//    public RegisterStoreResponseDto(Store storeEntity) {
-//        this.seq = storeEntity.getSeq();
-//        this.ownerSeq = storeEntity.getOwnerSeq();
-//        this.name = storeEntity.getName();
-//        this.businessReportCardImage = storeEntity.getBusinessReportCardImage();
-//        this.businessLicenseImage = storeEntity.getBusinessLicenseImage();
-//        this.businessName = storeEntity.getBusinessName();
-//        this.businessPersonName = storeEntity.getBusinessPersonName();
-//        this.businessNumber = storeEntity.getBusinessNumber();
-//        this.businessAddress = storeEntity.getBusinessAddress();
-//        this.address = storeEntity.getAddress();
-//        this.telNumber = storeEntity.getTelNumber();
-//        this.introduction = storeEntity.getIntroduction();
-//        this.openTime = storeEntity.getOpenTime();
-//        this.dayOff = storeEntity.getDayOff();
-//        this.originCountry = storeEntity.getOriginCountry();
-//        this.logo = storeEntity.getLogo();
-//        this.backgroundImage = storeEntity.getBackgroundImage();
-//        this.tableCount = storeEntity.getTableCount();
-//        this.category = storeEntity.getCategory();
-//        this.status = storeEntity.getStatus();
-//    }
+    Long seq;
+    Long ownerSeq;
+    String name;
+    String businessReportCardImageUrl;
+    String businessLicenseImageUrl;
+    String businessName;
+    String businessPersonName;
+    String businessNumber;
+    String businessAddress;
+    String address;
+    String telNumber;
+    String introduction;
+    String openTime;
+    String dayOff;
+    String originCountry;
+    String logoImageUrl;
+    String backgroundImageUrl;
+    Integer tableCount;
+    String category;
+    StoreStatus status;
+
+    public RegisterStoreResponseDto(Store storeEntity) {
+        this.seq = storeEntity.getSeq();
+        this.ownerSeq = storeEntity.getOwnerSeq();
+        this.name = storeEntity.getName();
+        this.businessReportCardImageUrl = storeEntity.getBusinessReportCardImageUrl();
+        this.businessLicenseImageUrl = storeEntity.getBusinessLicenseImageUrl();
+        this.businessName = storeEntity.getBusinessName();
+        this.businessPersonName = storeEntity.getBusinessPersonName();
+        this.businessNumber = storeEntity.getBusinessNumber();
+        this.businessAddress = storeEntity.getBusinessAddress();
+        this.address = storeEntity.getAddress();
+        this.telNumber = storeEntity.getTelNumber();
+        this.introduction = storeEntity.getIntroduction();
+        this.openTime = storeEntity.getOpenTime();
+        this.dayOff = storeEntity.getDayOff();
+        this.originCountry = storeEntity.getOriginCountry();
+        this.logoImageUrl = storeEntity.getLogoImageUrl();
+        this.backgroundImageUrl = storeEntity.getBackgroundImageUrl();
+        this.tableCount = storeEntity.getTableCount();
+        this.category = storeEntity.getCategory();
+        this.status = storeEntity.getStatus();
+    }
 }
