6179d324bdbe911329657a428226457555e5292c
GGoMak <kia0097@naver.com>
2022-11-13 15:33:49 +0900

    HANDY-51 Introduce Swagger

--- Store/src/main/java/com/jodongari/handy/controller/MenuController.java
+++ Store/src/main/java/com/jodongari/handy/controller/MenuController.java
@@ -1,19 +1,26 @@
 package com.jodongari.handy.controller;
 
-import com.jodongari.handy.protocol.api.ApiMessage;
+import com.jodongari.handy.protocol.api.ErrorResponse;
 import com.jodongari.handy.protocol.dto.request.GetMenuRequestDto;
 import com.jodongari.handy.protocol.dto.request.RegisterMenuRequestDto;
 import com.jodongari.handy.protocol.dto.response.GetMenuResponseDto;
 import com.jodongari.handy.protocol.dto.response.RegisterMenuResponseDto;
+import com.jodongari.handy.protocol.dto.response.RegisterStoreResponseDto;
 import com.jodongari.handy.protocol.url.MenuApiUrl;
 import com.jodongari.handy.service.MenuService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "메뉴", description = "메뉴 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -21,9 +28,14 @@ public class MenuController {
 
     private final MenuService menuService;
 
+    @Operation(summary = "메뉴 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetMenuResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping(value = MenuApiUrl.MENU_GET)
-    public ApiMessage<GetMenuResponseDto> getMenu(final GetMenuRequestDto request) throws Exception {
-        return ApiMessage.success(menuService.getMenu(request.getStoreSeq()));
+    public GetMenuResponseDto getMenu(GetMenuRequestDto request) throws Exception {
+        return menuService.getMenu(request.getStoreSeq());
     }
 
     //TODO - 22.10.22 이미지 저장 프로세스 정해지면 추가수정 예정
@@ -33,9 +45,13 @@ public class MenuController {
 //        return ApiMessage.success(menuService.registerMenu(request, imageFile));
 //    }
 
+    @Operation(summary = "메뉴 등록")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterMenuResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping(value = MenuApiUrl.MENU_REGISTER)
-    public ApiMessage<RegisterMenuResponseDto> registerMenu(@RequestPart final RegisterMenuRequestDto request) throws Exception {
-        return ApiMessage.success(menuService.registerMenu(request));
+    public RegisterMenuResponseDto registerMenu(RegisterMenuRequestDto request) throws Exception {
+        return menuService.registerMenu(request);
     }
-
 }
--- Store/src/main/java/com/jodongari/handy/controller/StoreController.java
+++ Store/src/main/java/com/jodongari/handy/controller/StoreController.java
@@ -1,5 +1,6 @@
 package com.jodongari.handy.controller;
 
+import com.jodongari.handy.protocol.api.ErrorResponse;
 import com.jodongari.handy.protocol.dto.request.GetStoreRequestDto;
 import com.jodongari.handy.protocol.dto.response.GetStoreResponseDto;
 import com.jodongari.handy.protocol.url.StoreApiUrl;
@@ -10,6 +11,13 @@ import com.jodongari.handy.protocol.dto.request.RegisterStoreRequestDto;
 import com.jodongari.handy.protocol.dto.response.ManageTableInfoResponseDto;
 import com.jodongari.handy.protocol.dto.response.RegisterStoreResponseDto;
 import com.jodongari.handy.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "매장", description = "매장 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -25,23 +34,43 @@ public class StoreController {
 
     private final StoreService storeService;
 
+    @Operation(summary = "매장 등록")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterStoreResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping(StoreApiUrl.STORE_REGISTER)
-    public ApiMessage<RegisterStoreResponseDto> registerStore(RegisterStoreRequestDto request) {
-        return ApiMessage.success(storeService.registerStore(request));
+    public RegisterStoreResponseDto registerStore(RegisterStoreRequestDto request) {
+        return storeService.registerStore(request);
     }
 
+    @Operation(summary = "매장 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetStoreResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping(StoreApiUrl.STORE_GET)
-    public ApiMessage<GetStoreResponseDto> getStore(GetStoreRequestDto request) {
-        return ApiMessage.success(storeService.getStore(request));
+    public GetStoreResponseDto getStore(GetStoreRequestDto request) {
+        return storeService.getStore(request);
     }
 
+    @Operation(summary = "매장 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = GetStoreResponseDto.class)))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping(StoreApiUrl.STORES_GET)
-    public ApiMessage<List<GetStoreResponseDto>> getStores(GetStoresRequestDto request) {
-        return ApiMessage.success(storeService.getStores(request));
+    public List<GetStoreResponseDto> getStores(GetStoresRequestDto request) {
+        return storeService.getStores(request);
     }
 
+    @Operation(summary = "테이블 관리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ManageTableInfoResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping(StoreApiUrl.MANAGE_TABLE_INFO)
-    public ApiMessage<ManageTableInfoResponseDto> manageTableInfo(ManageTableInfoRequestDto request) {
-        return ApiMessage.success(storeService.manageTableInfo(request));
+    public void manageTableInfo(ManageTableInfoRequestDto request) {
+        storeService.manageTableInfo(request);
     }
 }
