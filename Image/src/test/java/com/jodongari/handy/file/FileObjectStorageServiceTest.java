2c96dcfabe1c36317405bf4d7fd8763497fd2078
changwoo <qaqa313a@gmail.com>
2023-01-15 15:23:30 +0900

    HANDY-70 이미지 처리 테스트: 이미지 byte단위 비교 수정

--- Image/src/test/java/com/jodongari/handy/file/FileObjectStorageServiceTest.java
+++ Image/src/test/java/com/jodongari/handy/file/FileObjectStorageServiceTest.java
@@ -4,6 +4,8 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,15 +30,21 @@ class FileObjectStorageServiceTest {
     void putObject() throws IOException {
 
         String fileName = "test";
-        String contentType = "jpg";
-        String filePath = "src/main/resources/test.jpg";
+        String contentType = "jpeg";
+        String filePath = "src/main/resources/test.jpeg";
         MockMultipartFile imageFile = makeMultiPartFile(fileName, contentType, filePath);
 
         String imageKey = fileObjectStorageService.uploadObjectToS3(imageFile);
 
         S3Object o = s3.getObject(HANDY_IMAGE_BUCKET_NAME, imageKey);
-
         Assertions.assertEquals(imageKey, o.getKey());
+
+        S3ObjectInputStream s3is = o.getObjectContent();
+        File expectedFile = new File("src/main/resources/test.jpeg");
+        FileUtils.copyInputStreamToFile(imageFile.getInputStream(), expectedFile);
+        Assertions.assertEquals(FileUtils.readLines(expectedFile, "UTF-8"), FileUtils.readLines(expectedFile, "UTF-8"));
+        expectedFile.deleteOnExit();
+        s3is.close();
     }
 
     private MockMultipartFile makeMultiPartFile(String fileName, String contentType, String path) throws IOException {
