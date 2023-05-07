8d20dc7b858eb9515e03f1464886c25e5e58a46b
changwoo <qaqa313a@gmail.com>
2023-05-07 13:25:06 +0900

    HANDY-79 NO-ISSUE: change code

--- docker/mongo-init/mongo-init.js
+++ docker/mongo-init/mongo-init.js
@@ -1,1 +1,10 @@
 db = db.getSiblingDB('handy');
+db.createUser({
+    user: 'handy',
+    pwd: 'handy',
+    roles: [
+        {
+            role:'readWrite',
+            db:'handy'
+        }]
+});
