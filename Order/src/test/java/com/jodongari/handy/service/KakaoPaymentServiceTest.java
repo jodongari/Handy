d8a2f2411ea37fa4fb9aff81300aac1fd5dd015c
changwoo <qaqa313a@gmail.com>
2023-05-07 13:58:36 +0900

    HANDY-79 NO-ISSUE: change code

--- Order/src/test/java/com/jodongari/handy/service/KakaoPaymentServiceTest.java
+++ Order/src/test/java/com/jodongari/handy/service/KakaoPaymentServiceTest.java
@@ -1,10 +1,13 @@
 package com.jodongari.handy.service;
 
-import com.google.common.hash.Hashing;
 import com.jodongari.handy.domain.Order;
 import com.jodongari.handy.infrastructure.repository.redis.PaymentSessionRedisRepository;
 import com.jodongari.handy.protocol.dto.ReadyPaymentRequest;
+import com.jodongari.handy.protocol.dto.kakao.ApproveKakaoPaymentResponse;
 import com.jodongari.handy.protocol.dto.kakao.ReadyKakaoPaymentResponse;
+import com.jodongari.handy.protocol.model.MenuModel;
+import com.jodongari.handy.protocol.model.MenuOptionModel;
+import com.jodongari.handy.protocol.session.KakaoPaymentSession;
 import com.jodongari.handy.service.impl.KakaoPaymentServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +16,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
@@ -28,6 +32,9 @@ public class KakaoPaymentServiceTest {
     private OrderDomainService orderDomainService;
 
     @Mock
+    private SessionDomainService sessionDomainService;
+
+    @Mock
     private ModelMapper modelMapper;
 
     @Mock
@@ -37,23 +44,54 @@ public class KakaoPaymentServiceTest {
     private KakaoPaymentServiceImpl cut;
 
     private String partnerOrderId = "partnerOrderId";
-    private String partnerUserId = "partnerUserId";
+    private Long partnerUserId = 1L;
 
     @Test
     public void readyKakaoPayment_success() {
+        final MenuOptionModel menuOption = new MenuOptionModel();
+        menuOption.setSeq(1L);
+        menuOption.setName("소");
+        menuOption.setPrice(1000);
+        final MenuModel menu = new MenuModel();
+        menu.setSeq(1L);
+        menu.setCount(1);
+        menu.setName("떡볶이");
+        menu.setMenuOption(menuOption);
         final Order order = Order.builder()
-                .orderSeq(Hashing.sha256().hashString(UUID.randomUUID().toString(), StandardCharsets.UTF_8).toString())
-                .userSeq(1L)
+                .orderSeq(partnerOrderId)
+                .userSeq(partnerUserId)
                 .storeSeq(1L)
+                .menus(List.of(menu))
                 .build();
 
         when(modelMapper.map(any(), any()))
-                .thenReturn(order);
-        when(orderDomainService.registerOrder(any()))
                 .thenReturn(order);
         when(kakaoApiService.callKakaoPaymentReady(any(), any(), any(), any()))
                 .thenReturn(new ReadyKakaoPaymentResponse());
 
         cut.readyKakaoPayment(new ReadyPaymentRequest());
+    }
+
+    @Test
+    public void approveKakaoPayment_success() {
+        final Order order = Order.builder()
+                .orderSeq(partnerOrderId)
+                .userSeq(partnerUserId)
+                .storeSeq(1L)
+                .build();
+        final KakaoPaymentSession kakaoPaymentSession = KakaoPaymentSession.builder()
+                .tid("")
+                .partnerUserId(Long.toString(partnerUserId))
+                .partnerOrderId(partnerOrderId)
+                .build();
+
+        when(sessionDomainService.getSession(anyString(), eq(KakaoPaymentSession.class)))
+                .thenReturn(kakaoPaymentSession);
+        when(orderDomainService.registerOrder(any()))
+                .thenReturn(order);
+        when(kakaoApiService.callKakaoPaymentApprove(any(), any(), any(), any()))
+                .thenReturn(new ApproveKakaoPaymentResponse());
+
+        cut.approveKakaoPayment(new String(), new String());
     }
 }
